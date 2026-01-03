b42ca401f79a5f7bc743d1104cb11155bd0bfa8e
Nobody <sonblue34@gmail.com>
2026-01-02 21:39:48 -0600

    Auto-backup: 2026-01-02 21:39:48

---
+++ version.py
@@ -0,0 +1,10 @@
+"""
+RPG Shared Database Version Information
+"""
+
+# Database Version (Format: MAJOR.MINOR.PATCH-beta)
+# Increment PATCH (x in 1.0.x) with each database schema change
+# Beta status will be maintained until public release
+DATABASE_VERSION = "1.0.0-beta"
+DATABASE_NAME = "RPG Shared Database"
+DATABASE_DESCRIPTION = "Shared database system for RP.env roleplay bots"
